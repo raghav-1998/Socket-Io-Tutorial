cf222c155a0ad0649bc9896bd11cfa1ac39f13b1
Raghav Vasdeva <raghav.vasdeva@gmail.com>
2024-09-11 17:30:39 +0530

    Setup of Socket Io Tutorial Project

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { Server } from "socket.io";
+import http from 'http';
+
+const app=express();
+const PORT=8000;
+const server=http.createServer(app);
+const io= new Server(server);
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
+app.get('/',(req,res)=>{
+    //res.send('Hello World');
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+io.on('connection', (socket)=>{
+    console.log("User Connected");
+
+    socket.on('chat message', (msg)=>{
+        console.log('message:',msg);
+        io.emit(msg);
+
+    })
+
+    socket.on('disconnected', ()=>{
+        console.log('User Disconnected');
+    });
+})
+server.listen(PORT,()=>{
+    console.log("App is listening on",PORT);
+})
